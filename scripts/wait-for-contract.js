0c9c95f90aee1bca0d5f116597b88484bc9183e6
Yusef Napora <yusef@protocol.ai>
2021-11-01 21:24:15 -0400

    add scripts to start dev env reliably
    the previous "just sleep 5 seconds" approach falls down if you need
    to download a solidity compiler :)

---
+++ scripts/wait-for-contract.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const fs = require('fs/promises')
+const { F_OK } = require('fs')
+
+async function exists(filename) {
+  try {
+    await fs.access(filename, F_OK)
+    return true
+  } catch (err) {
+    return false
+  }
+}
+
+
+async function waitForContract() {
+  const outputFile = path.join(__dirname, '..', 'packages', 'webapp', 'src', 'contracts', 'hardhat_contracts.json')
+  const maxWait = 300
+  for (let i = 0; i < maxWait; i++) {
+    if (await exists(outputFile)) {
+      return
+    }
+  }
+  throw new Error(`Contract output did not appear after ${maxWait} seconds`)
+}
+
+waitForContract().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
